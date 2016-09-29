c1c99db3a5d60fefb74fc8e6d4a249032ab746fe
Spencer Tranter <spencertranter95@gmail.com>
2016-09-29 21:34:14 +0000

    Created new user route

--- routes/users.js
+++ routes/users.js
@@ -5,24 +5,11 @@ const router  = express.Router();
 
 
 module.exports = (knex) => {
-  const userMethods = require('../db/methods/users.js')(knex);
+  //const userMethods = require('../db/methods/users.js')(knex);
 
-  router.get("/", (req, res) => {
-    userMethods.getUser(1, (err, user) => {
-      if (err) return console.log(err);
-      console.log(user);
-    });
-  });
-  return router;
-
-  // router.get("/", (req, res) => {
-  //   knex
-  //     .select("*")
-  //     .from("users")
-  //     .then((results) => {
-  //       res.json(results);
-  //   });
-  // });
+  router.get("/user/new"){
+    res.render("create_user")
+  }
 
 
   return router;
