71544166077ec9fd77b0dcb8181e64fe5218edb6
Spencer Tranter <spencertranter95@gmail.com>
2016-10-01 21:25:07 +0000

    Create users with four appropriate list attached to them

--- routes/users.js
+++ routes/users.js
@@ -6,26 +6,45 @@ const express = require('express');
 
 module.exports = (app, knex) => {
   const userMethods = require('../db/methods/users.js')(knex);
+  const listMethods = require('../db/methods/lists.js')(knex);
 
   app.get("/user/new", (req, res) => {
     res.render("create_users");
   });
 
   app.post("/users/new", (req, res) => {
+    let user_email = req.body.email;
     let user = {
-        first_name: req.body.firstName,
-        last_name: req.body.lastName,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
         email: req.body.email,
         password: req.body.password
       }
       userMethods.insertUser(user, (err, result) => {
         if (err) return console.log(err);
-        //userMethods.getUsers((err,result) => {
-         // console.log(result);
+
+        userMethods.getUserByEmail(user_email, (err,result) => {
+          let result_id = JSON.stringify(result[0].id);
+          listMethods.insertList(create_list('restaurants', result_id), (err, result) => {
+            if (err) return console.log(err);
+          });
+          listMethods.insertList(create_list('movies', result_id), (err, result) => {
+            if (err) return console.log(err);
+          });
+          listMethods.insertList(create_list('purchases', result_id), (err, result) => {
+            if (err) return console.log(err);
+          });
+          listMethods.insertList(create_list('books', result_id), (err, result) => {
+            if (err) return console.log(err);
+          });
+          console.log('User Created!');
           res.redirect('/login');
-        //});
+        });
       });
   });
 
+  let create_list = function(list_type, list_user_id){
+    return {type: list_type, user_id: list_user_id}
+  }
   // return router;
 }
