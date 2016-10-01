d9dc9f99bb04359744dc33b312061f592efadceb
to-y <yitong922@gmail.com>
2016-10-01 04:14:44 +0000

    data posted from user chosen checkboxes and inserted but not into unique lists

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,7 @@ $(() => {
   $('.email').focus();
   $('#email').focus();
 
-$('.dropdown-menu').dropdown();
+  $('.dropdown-menu').dropdown();
 
   //Passes logout call from button click to login.js
   $('#logout_form').on('submit', function (event){
@@ -15,14 +15,23 @@ $('.dropdown-menu').dropdown();
     })
   })
 
- $('#user_choice').on('submit', function (event) {
-  event.preventDefault();
-  $.ajax({
-    url:'/insertItem',
-    method:'POST',
-    success: function(data) {
-      console.log(data);
-    }
+  $('#user_choice').on('submit', function (event) {
+    event.preventDefault();
+    var formData = {};
+    $('.cbox:checked').each((index, element) => {
+      formData[element.dataset.category] = $(element).text();
+    });
+    console.log("I'm going to send this to your mom: ", formData);
+    $.ajax({
+      url:'/insertItem',
+      method:'POST',
+      data: formData,
+      success: function (err, data) {
+        console.log(err, data);
+        // todo: update the page
+      }
+    })
   })
- })
 });
+
+
--- routes/index.js
+++ routes/index.js
@@ -7,29 +7,40 @@ const router  = express.Router();
 module.exports = (app, knex) => {
   const itemMethods = require('../db/methods/items.js')(knex);
 
-app.get("/", (req, res) => {
-  if (!req.user){
-    res.redirect('/login');
-    return;
-  }
-  console.log(req.user)
-    itemMethods.getAll(req.user[0].id, (err, all) => {
-      if (err) return console.log(err);
-      res.render("index", {
-        all: all,
-        user_id: req.user.id
+  app.get("/", (req, res) => {
+    if (!req.user){
+      res.redirect('/login');
+      return;
+    }
+    console.log(req.user)
+      itemMethods.getAll(req.user[0].id, (err, all) => {
+        if (err) return console.log(err);
+        res.render("index", {
+          all: all,
+          user_id: req.user.id
+        })
       })
-    })
-});
-
-app.post("/insertItem", (req, res) => {
-    console.log(req.user.id);
-    console.log(req.body);
-
   });
 
-//req.user.id grabs id grabs id
+  app.post("/insertItem", (req, res) => {
+    let userId = req.user[0].id
+    let types = Object.keys(req.body);
+    let object = req.body;
 
-return router;
+    itemMethods.getListId(userId, (err, info) => {
+    if (err) return console.log(err);
+      console.log(info);
+    });
+
+    types.forEach(function (type) {
+      let lowerType = type.toLowerCase();
+      // itemMethods.insertItem(userId, type, object[lowerType]); //works and inserts into db
+    });
+    // todo: save to correct dB, and if successful, return a 200 or something
+  });
+
+  //req.user.id grabs id grabs id
+
+  return router;
 }
 
