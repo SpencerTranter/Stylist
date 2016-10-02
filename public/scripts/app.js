23d178f1105bd525ed4cbe003cda7ec2d6273b0f
Spencer Tranter <spencertranter95@gmail.com>
2016-10-02 23:51:01 +0000

    Fixed duplicate insert button bug

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -29,36 +29,11 @@ $(() => {
       url:'/insertItem',
       method:'POST',
       data: formData,
-      success: checked.each(function append_to_list(index, element) {
-        var  type = element.dataset.category
-        var name = $(element).text();
-        let delete_button =
-          `<form class'delete_item' method='POST' action='/delete/<%=item.id%>?_method=DELETE>
-            <button class='delete_button' type='submit' aria-hidden=true><i class='fa fa-trash' aria-hidden=true></i></button>
-            ${name}
-          </form>`
-
-        if (type === 'Movie') {
-          $("<li>" + delete_button + "</li>")
-          .appendTo(".list-unstyled.movie");
-        } else if (type === 'Book') {
-          $("<li>" + delete_button + "</li>")
-          .appendTo(".list-unstyled.book");
-        } else if (type === 'Purchase') {
-          $("<li>" + delete_button + "</li>")
-          .appendTo(".list-unstyled.purchase");
-        } else if (type === 'Restaurant') {
-          $("<li>" + delete_button + "</li>")
-          .appendTo(".list-unstyled.restaurant");
-        }
-
-      }),
       success: (itemID) => {
         checked.each(function append_to_list(index, element) {
           var  type = element.dataset.category
           console.log(element);
           var name = $(element).text();
-          //let delete_button = "<form class=\"delete_item\" method=\"POST\" action=\"/delete/<%=item.id%>?_method=DELETE\"> <button class=\"delete_button\" type=\"submit\" aria-hidden=true><i class=\"fa fa-trash\" aria-hidden=true></i></button></form>"
           let delete_button =
             `<form class='delete_item' method='DELETE' action='/delete/${itemID}'>
               <button class="delete_button" type="submit"><i class='fa fa-trash' aria-hidden=true></i></button>
