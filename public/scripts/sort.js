80497d32b7c7c135f9b6c8aac60ac667d34fcb9d
to-y <yitong922@gmail.com>
2016-10-02 10:30:03 +0000

    fixed drag going behind pics, kinda worked around dragging into empty list

--- public/scripts/sort.js
+++ public/scripts/sort.js
@@ -1,8 +1,15 @@
 
   $(function(){
 
-    $( ".restaurant, .movie, .purchase, .book" ).sortable({
+    $( ".list-unstyled" ).sortable({
       connectWith: ".list-unstyled",
+      revert: true,
+      scroll: false,
+      dropOnEmpty: true,
+      appendTo: '.lists',
+      helper: 'clone',
+
+
       stop: function(event, ui) {
         let parentName = event.target.parentElement.className;
         let targetName = ui.item[0].offsetParent.className;
